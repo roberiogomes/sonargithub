bd9638dee79fea37ea3db8a6815145e92fd161c3
Robério Gomes Patricio <roberiogomes@gmail.com>
2021-12-29 09:54:53 -0300

    Initial version

---
+++ src/main/java/com/example/sonargithub/SonargithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonargithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonargithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonargithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonargithub/SonargithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonargithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonargithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
